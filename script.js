469baa4a75d67e92fef45bcbaaf1abbec1b2967a
SergioLNeves <sergioluiz852@gmail.com>
2023-05-06 04:02:20 -0300

    carousel pronto

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
 $(window).scroll(function () {
   var height = $(window).scrollTop();
   if (height > 100) {
-    $('#navbarElement').removeClass('backgroundNavbar').addClass('bg-white');
-    $('#navbarLogo').removeClass('logo-white').addClass('logo-black');
+    $("#navbarElement").removeClass("backgroundNavbar").addClass("bg-white");
+    $("#navbarLogo").removeClass("logo-white").addClass("logo-black");
   } else {
-    $('#navbarElement').removeClass('bg-white').addClass('backgroundNavbar');
-    $('#navbarLogo').removeClass('logo-black').addClass('logo-white');
+    $("#navbarElement").removeClass("bg-white").addClass("backgroundNavbar");
+    $("#navbarLogo").removeClass("logo-black").addClass("logo-white");
   }
 });
-
 
 function toggleDropdown() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     for (var i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
-}
+};
 
-$(document).ready(function(){
-  $('.slider').slick();
+$(document).ready(function () {
+  $(".slider").slick({
+    autoplay: true,
+    infinite: true,
+    autoplaySpeed: 5000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  });
 });
+
